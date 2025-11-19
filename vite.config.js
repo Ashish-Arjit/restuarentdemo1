8776d027aa19b9d4e96d6df4a3f280b0132ada59
Ashish-Arjit <btashish999@gmail.com>
2025-11-19 10:39:39 +0530

    Update and rename vite.config.ts to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
@@ -15,4 +14,7 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    chunkSizeWarningLimit: 1600,  // increase this from default 500kb to 1600kb (1.6MB)
+  }
 }));
